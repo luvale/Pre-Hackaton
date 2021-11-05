38ce17838c1d8046ab5206c386415c5ad9dde219
luvale <valeriae200@hotmail.com>
2021-11-05 14:16:00 -0500

    install ng zorro

--- preHackaton/src/app/app.module.ts
+++ preHackaton/src/app/app.module.ts
@@ -7,6 +7,15 @@ import { AppComponent } from './app.component';
 // Nuestros componentes
 import { SearchComponent } from './components/search-vale/search.component';
 import { MenuEliPComponent } from './components/menu-eli-p/menu-eli-p.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { es_ES } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import es from '@angular/common/locales/es';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+registerLocaleData(es);
 
 @NgModule({
   declarations: [
@@ -16,9 +25,12 @@ import { MenuEliPComponent } from './components/menu-eli-p/menu-eli-p.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: es_ES }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
