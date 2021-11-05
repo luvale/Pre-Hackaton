9bbfb226e26c4536868a04753a67291baa70f8f6
luvale <valeriae200@hotmail.com>
2021-11-05 11:09:11 -0500

    search es ahora search-vale

--- preHackaton/src/app/app.module.ts
+++ preHackaton/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SearchComponent } from './components/search/search.component';
+import { SearchComponent } from './components/search-vale/search.component';
 
 @NgModule({
   declarations: [
